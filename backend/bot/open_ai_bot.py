265abf9561c66ca22a3b8c0edc063270eafc987c
Henry <oncwnuJh2IqccX_GEW8cN-lM9-TQ@git.weixin.qq.com>
2023-03-03 10:23:05 +0800

    new

--- backend/bot/open_ai_bot.py
+++ backend/bot/open_ai_bot.py
@@ -1,5 +1,4 @@
 from bot.bot import Bot
-from utils.logger import logger
 import openai
 import os
 
@@ -9,9 +8,7 @@ class OpenAIBot(Bot):
         openai.api_key = os.environ['VITE_OPENAI_API_KEY']
 
     def reply(self, query):
-        logger.debug("[OPEN_AI] session query={}".format(query))
         success, reply_content = self.reply_text(query)
-        logger.debug("[OPEN_AI] query={}, reply_cont={}".format(query, reply_content))
         return success, reply_content
 
     def reply_text(self, content):
@@ -21,17 +18,9 @@ class OpenAIBot(Bot):
                 messages=[{'role': "user", "content": content}]
             )
             res_content = response['choices'][0]['message']['content'].strip()
-            prompt_tokens = response['usage']['prompt_tokens']
-            completion_tokens = response['usage']['completion_tokens']
-            total_tokens = response['usage']['total_tokens']
-            logger.info(
-                f"[OPEN_AI] prompt_tokens={prompt_tokens} completion_tokens={completion_tokens} total_tokens={total_tokens}")
-            logger.debug("[OPEN_AI] reply={}".format(res_content))
             return True, res_content
         except openai.error.RateLimitError as e:
             # rate limit exception
-            logger.error(e)
             return False, "提问太快啦，请休息一下再问我吧"
         except Exception as e:
-            logger.exception(e)
             return False, "请再问我一次吧"
--- backend/controller/api/chatGPT.py
+++ backend/controller/api/chatGPT.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Body
 from response_model.base_resm import BaseRes
-from utils.logger import logger
 from bot import bot_factory
-import traceback
 
 router = APIRouter()
 
@@ -10,9 +8,7 @@ router = APIRouter()
 @router.post('/correction/', response_model=BaseRes)
 def correction(content: str = Body(..., embed=True)) -> BaseRes:
     try:
-        response = bot_factory.create_bot().reply("修改错别字: " + content)
+        response = bot_factory.create_bot().reply(content)
         return BaseRes(data=response)
     except Exception as e:
-        logger.error(e)
-        logger.error(traceback.format_exc())
         return BaseRes(status=0, error=str(e))
